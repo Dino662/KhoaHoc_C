048a1c40ad441d103b99ea40824993d2bb5455d9
Dino662 <84856006+Dino662@users.noreply.github.com>
2021-06-11 20:59:42 +0700

    DoNguyenGiaHung_0355595005

---
+++ Buoi7_C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Buoi7_C
+{
+    class Buoi7_C
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
